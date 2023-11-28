a767ed748adc8ee7926770fb17af5b1f921058ea
unknown <amir.maalaoui27@gmail.com>
2023-11-28 22:04:08 +0100

    first commit

---
+++ src/app/models/product.ts
@@ -0,0 +1,17 @@
+export class Product{
+     id : number ;
+     title : string ;
+     price : number ;
+     quantity : number ;
+     like : number ;
+    // the constructor 
+    constructor(id : number,title : string,price : number,quantity : number , like : number){
+        this.id = id ; 
+        this.title = title ; 
+        this.price = price ; 
+        this.quantity = quantity ; 
+        this.like = like ; 
+ 
+    } 
+
+}
